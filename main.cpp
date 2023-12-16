21fd7379d69b68f5c1ddbdac89622c109c999783
Snail Mail <>
2023-12-15 18:04:54 -0600

    refactor

--- main.cpp
+++ main.cpp
@@ -16,45 +16,47 @@ struct Element {
   Duration duration;
 };
 
-const int pattern_row = 7;
+const int pattern_top = 7;
 const int pattern_left = 5;
 
 std::vector<Element> pattern;
 
-void draw_element(Element el, int &col) {
-  char ch = el.type == Note ? 'x' : '.';
-  int len;
+int get_element_len(Element el) {
   switch (el.duration) {
   case D16:
-    len = 1;
-    break;
+  default:
+    return 1;
   case D8:
-    len = 2;
-    break;
+    return 2;
   case D4:
-    len = 4;
-    break;
+    return 4;
   }
+}
+
+void draw_element(Element el, int &col) {
+  char ch = el.type == Note ? '=' : '.';
+  int len = get_element_len(el);
 
   for (int j = 0; j < len; j++) {
-    mvaddch(pattern_row, pattern_left + col + j, ch);
+    mvaddch(pattern_top, pattern_left + col + j, ch);
   }
 
-  mvaddch(pattern_row, pattern_left + col + len, '|');
+  mvaddch(pattern_top, pattern_left + col + len, '|');
   col += len + 1;
 }
 
 int main() {
 
   initscr();
-  raw();                /* Line buffering disabled	*/
-  keypad(stdscr, TRUE); /* We get F1, F2 etc..		*/
-  noecho();             /* Don't echo() while we do getch */
+  raw();                // Line buffering disabled
+  keypad(stdscr, TRUE); // We get F1, F2 etc..
+  noecho();             // Don't echo() while we do getch
+  curs_set(0);          // Hide cursor
 
   mvprintw(0, 0, "-- Pattern Editor --");
-  mvprintw(2, 0, "  Note: 1/4 - q, 1/8 - w, 1/16 - e");
-  mvprintw(3, 0, " Pause: 1/4 - a, 1/8 - s, 1/16 - d");
-  mvprintw(5, 0, " z - delete last, r - render, x - exit");
+  mvprintw(2, 0, "     Note: 1/4 - q, 1/8 - w, 1/16 - e");
+  mvprintw(3, 0, "    Pause: 1/4 - a, 1/8 - s, 1/16 - d");
+  mvprintw(5, 0, " Commands: z - delete last, r - render, x - exit");
   refresh();
 
   int ch;
@@ -80,17 +82,25 @@ int main() {
     case 'd':
       pattern.push_back({Pause, D16});
       break;
+    case 'z':
+      if (pattern.size() > 0)
+        pattern.pop_back();
+      break;
     }
 
     // Draw the pattern
-    int col = 0;
-    mvaddch(pattern_row, pattern_left - 1, '|');
+    move(pattern_top, 0); // move to begining of line
+    clrtoeol();           // clear line
 
-    for (size_t i = 0; i < pattern.size(); i++) {
-      Element e = pattern[i];
-      draw_element(e, col);
+    if (pattern.size() > 0) {
+      int col = 0;
+      mvaddch(pattern_top, pattern_left - 1, '|');
+
+      for (size_t i = 0; i < pattern.size(); i++) {
+        Element e = pattern[i];
+        draw_element(e, col);
+      }
     }
-
     refresh();
 
   } while (ch != 'x');
